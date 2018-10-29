957dcab4c34c18919350885f88d51fa219a04eba
Leandro França <leandrofranca@gmail.com>
2018-10-29 01:13:35 -0300

    Ajuste de identação

--- options.js
+++ options.js
@@ -1,16 +1,16 @@
 let page = document.getElementById('buttonDiv');
-  const tipoJornada = ['6h', '8h', 'Ambas'];
-  function constructOptions(tipoJornada) {
-    for (let item of tipoJornada) {
-      let button = document.createElement('button');
-      var text = document.createTextNode(item);
-      button.appendChild(text);            
-      button.addEventListener('click', function() {
-        chrome.storage.sync.set({jornada: item}, function() {
-          console.log('jornada escolhida ' + item);
-        })
-      });
-      page.appendChild(button);
-    }
+const tipoJornada = ['6h', '8h', 'Ambas'];
+function constructOptions(tipoJornada) {
+  for (let item of tipoJornada) {
+    let button = document.createElement('button');
+    var text = document.createTextNode(item);
+    button.appendChild(text);
+    button.addEventListener('click', function () {
+      chrome.storage.sync.set({ jornada: item }, function () {
+        console.log('jornada escolhida ' + item);
+      })
+    });
+    page.appendChild(button);
   }
-  constructOptions(tipoJornada);
+}
+constructOptions(tipoJornada);
